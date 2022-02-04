4f3037920983912a729322509a8e0c499b13128c
fatihhafızoğlu <fatih199832@hotmail.com>
2022-02-05 01:25:54 +0300

    Side movement Lerp

--- Bootcamp-GraduationProject/Assets/Game/Scripts/PlayerController.cs
+++ Bootcamp-GraduationProject/Assets/Game/Scripts/PlayerController.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Transform sideMomentRoot;
+    [SerializeField] private Transform sideMovementRoot;
     [SerializeField] private Transform leftLimit, rightLimit;
     [SerializeField] private float forwardMovementSpeed = 1f, sideMomentSensitivity = 1f;
+    [SerializeField] private float characterSpeed = 5f;
+    [SerializeField] private float sideMovementLerpSpeed = 20f;
+
     private Vector2 inputDrag;
     private Vector2 previousMousePosition;
 
     private float leftLimitX => leftLimit.localPosition.x; 
     private float rightLimitX => rightLimit.localPosition.x;
+    private float sideMovementTarget = 0f;
+
+    private Vector2 mousePositionCM
+    {
+        get
+        {
+            Vector2 pixels = Input.mousePosition;
+            var inches = pixels / Screen.dpi;
+            var centimeters = inches * 2.54f;
+
+            return centimeters;
+        }
+    }
     void Start()
     {
         
@@ -27,31 +43,43 @@ public class PlayerController : MonoBehaviour
 
     private void HandleForwardMovement()
     {
-        transform.Translate(Vector3.forward * forwardMovementSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * forwardMovementSpeed * Time.deltaTime);
+        transform.position += transform.forward * Time.deltaTime * characterSpeed; 
     }
 
     private void HandleSideMovement()
     {
-        var localPos = sideMomentRoot.localPosition;
-        localPos += Vector3.right * inputDrag.x * sideMomentSensitivity;
+        sideMovementTarget += inputDrag.x * sideMomentSensitivity;
+        sideMovementTarget = Mathf.Clamp(sideMovementTarget, leftLimitX, rightLimitX);
+
+        var localPos = sideMovementRoot.localPosition;
+
+        //localPos += Vector3.right * inputDrag.x * sideMomentSensitivity;
         
-        localPos.x = Mathf.Clamp(localPos.x, leftLimitX, rightLimitX);
+        localPos.x = Mathf.Lerp(localPos.x, sideMovementTarget, Time.deltaTime * sideMovementLerpSpeed);
         
 
-        sideMomentRoot.localPosition = localPos;
+        sideMovementRoot.localPosition = localPos;
+
+        /*var moveDirection = Vector3.forward * forwardMovementSpeed * Time.deltaTime;
+        moveDirection += sideMovementRoot.right * inputDrag.x * sideMomentSensitivity;
+
+        moveDirection.Normalize();
+
+        sideMovementRoot.rotation = Quaternion.LookRotation(moveDirection, Vector3.up); */
     }
 
     private void HandleInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            previousMousePosition = Input.mousePosition;
+            previousMousePosition = mousePositionCM;
         }
         if(Input.GetMouseButton(0))
         {
-            var deltaMouse = (Vector2)Input.mousePosition - previousMousePosition;
+            var deltaMouse = mousePositionCM - previousMousePosition;
             inputDrag = deltaMouse;
-            previousMousePosition = Input.mousePosition;
+            previousMousePosition = mousePositionCM;
         }
         else 
         {
