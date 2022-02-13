0a23c90b0e8e87bf2e499f9eb5eb15086c7782af
fatihhafızoğlu <fatih199832@hotmail.com>
2022-02-13 21:31:30 +0300

    Revert "finishline texure aded"

--- Bootcamp-GraduationProject/Assets/Game/Scripts/RidingCylinder.cs
+++ Bootcamp-GraduationProject/Assets/Game/Scripts/RidingCylinder.cs
@@ -7,27 +7,7 @@ public class RidingCylinder : MonoBehaviour
    private bool filled;
    private float _value;
 
-<<<<<<< Updated upstream
     public void IncrementCylinderVolume(float value)
-=======
-<<<<<<< HEAD
-public void IncrementCylinderVolume(float value)
-{
-    _value += value;
-
-    if(_value >= 1)
-    {
-        float leftValue = _value - 1;
-        int cylinderCount = PlayerController.current.cylinders.Count;
-        transform.localPosition = new Vector3(transform.localPosition.x, -0.5f * (cylinderCount - 1) -0.25f, transform.localPosition.z);
-        transform.localScale = new Vector3(0.5f, transform.localScale.y, 0.5f);
-        PlayerController.current.CreateCylinder(leftValue);
-    }
-    else if(_value < 0)
-=======
-    public void IncrementCylinderVolume(float value)
->>>>>>> 4b423b30cd8906d37426144098f437d4ba2099be
->>>>>>> Stashed changes
     {
         _value += value;
 
